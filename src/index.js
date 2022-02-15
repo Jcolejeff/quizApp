65e5adcafae7a20d6c3659798bd3ebdf41a0da75
jcolejeff <ikwuhj@gmail.com>
2022-02-15 11:13:59 +0100

    setting up dev enviroment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h3>setup</h3>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
